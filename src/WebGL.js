fb023cd926518e4a53153f3c9fece2b9fbd8b9ac
Yuzhou Feng <Aaron@Yuzhous-MacBook-Pro.local>
2017-09-17 14:53:48 -0400

    reorganize code.

--- src/WebGL.js
+++ src/WebGL.js
@@ -1,8 +1,24 @@
+"use strict";
 
-var canvas = document.getElementById("GLCanvas");
-var gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+var gGL = null;
 
-if(gl !== null) {
-	gl.clearColor(0.0, 0.8, 0.0, 1.0);
-	gl.clear(gl.COLOR_BUFFER_BIT);
+var initializeGL = function() {
+	var canvas = document.getElementById("GLCanvas");
+	var gGL = canvas.getContext("webgl") || 
+			canvas.getContext("experimental-webgl");
+	if(gGL !== null) {
+		gGL.clearColor(0.0, 0.8, 0.0, 1.0);
+		gGL.clear(gGL.COLOR_BUFFER_BIT);
+	}
+	else {
+		document.write("<br><b>WebGL is not supported!</b>");
+	}
+}
+
+var clearCanvas = function() {
+}
+
+var doGLDraw = function() {
+	initializeGL();
+	clearCanvas();
 }
